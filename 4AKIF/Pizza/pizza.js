265df11bcda3eab9224cf24b73765bee6c4b722c
Christop98 <WEG240780@spengergasse.at>
2025-03-24 22:02:15 +0100

    Kata

---
+++ 4AKIF/Pizza/pizza.js
@@ -0,0 +1,40 @@
+class pizza
+{
+    constructor(name,price,isVeggietarian)
+    {
+        this.name = name;
+        this.price = price;
+        this.isVeggietarian = isVeggietarian;
+    }
+    
+}
+
+    let arr = [];
+    
+    arr.push(new pizza("Margherita", 100, true));
+    arr.push(new pizza("Marinara", 150, true));     
+    arr.push(new pizza("Quattro Stagioni", 200, true));
+    arr.push(new pizza("Carbonara", 250, false));
+    arr.push(new pizza("Frutti di Mare", 300, false));
+    arr.push(new pizza("Quattro Formaggi", 350, true));
+    arr.push(new pizza("Crudo", 400, false));
+    
+    console.log("Pizzas:", arr);
+    arr.reverse();
+    console.log("Pizzas reversed:", arr);
+   // arr.sort((pizza_a, pizza_b) =>{return pizza_a.price - pizza_b.price});
+    arr.sort(compare);
+    console.log("Pizzas sorted:", arr);
+
+    function compare(pizza_a, pizza_b)
+{
+    if(pizza_a.price < pizza_b.price)
+    {
+        return -1;
+    }
+    if(pizza_a.price > pizza_b.price)
+    {
+        return 1;
+    }
+    return 0 ;
+}
